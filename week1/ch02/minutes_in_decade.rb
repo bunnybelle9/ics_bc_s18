6a596ee86fff081cf16b00487d3175c37c8c76fd
bunnybelle9 <kerstenwillgohs@gmail.com>
2018-06-20 13:53:07 -0700

    Complete minutes_in_decade exercise and maybe also the lab

---
+++ week1/ch02/minutes_in_decade.rb
@@ -0,0 +1,1 @@
+puts "In a decade with two leap years, there are " + (2*366*24*60 + 8*365*24*60) + " minutes. In a decade with three leap years, there are " + (3*366*24*60 + 7*365*24*60) + " minutes."
