bf9e282024b7ee2b1994766b836ca837b8aa50c3
bunnybelle9 <kerstenwillgohs@gmail.com>
2018-06-29 18:25:53 -0700

    edited leap_year_counter exercise to be neater

--- week2/ch07/leap_year_counter.rb
+++ week2/ch07/leap_year_counter.rb
@@ -1,13 +1,13 @@
 puts "What is the starting year?"
-start_year = gets.chomp
+start_year = gets.chomp.to_i
 puts "What is the ending year?"
-end_year = gets.chomp
+end_year = gets.chomp.to_i
 puts
 year = start_year
 while year <= end_year
   if (year.to_i % 4) == 0
     if (year.to_i % 100) == 0
-      if (year.to_i % 400) == 0
+	    if (year.to_i % 400) == 0
 	      puts year
 	    else
 	    end
@@ -16,5 +16,5 @@ while year <= end_year
 	  end
   else
   end
-  year = (year.to_i + 1).to_s
+  year = year + 1
 end
